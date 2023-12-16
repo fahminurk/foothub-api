67140d856b5b11956131e9345331f57a070f4536
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-16 20:20:55 +0700

    update: add serverstatic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './core/user/user.module';
 import { AuthModule } from './core/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -10,6 +12,9 @@ import { PrismaModule } from './prisma/prisma.module';
     AuthModule,
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+    }),
   ],
   controllers: [],
   providers: [],
