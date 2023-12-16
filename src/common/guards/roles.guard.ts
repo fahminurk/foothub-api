5cff94f9167ebdfdecdfa4597829b0b8a3f7ff43
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-16 13:55:53 +0700

    feat: add role guard

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from '../enum/role.enum';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    // console.log(user);
+    const isTrue = roles.some((role) => user.role?.includes(role));
+    // console.log(isTrue);
+
+    return isTrue;
+  }
+}
