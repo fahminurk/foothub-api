9f79e57c8de916d9c9b03e1aaeaf94231d9f7548
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 16:59:25 +0700

    feat: add brand controller

--- src/core/brand/brand.controller.ts
+++ src/core/brand/brand.controller.ts
@@ -1,7 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { BrandService } from './brand.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { CreateBrandDto } from './brand.dto';
 
 @Controller('brand')
 export class BrandController {
   constructor(private readonly brandService: BrandService) {}
+
+  @Get()
+  findAll() {
+    return this.brandService.getAllBrands();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.brandService.getBrandById(id);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/static/brand',
+        filename(req, file, cb) {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  create(
+    @Body() data: CreateBrandDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'image/*' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.brandService.createBrand(data, file);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.brandService.deleteShoe(id);
+  }
 }
