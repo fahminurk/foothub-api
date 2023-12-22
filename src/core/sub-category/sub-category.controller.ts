19777e45902062dc6c7cb6fc06b89160a7b10879
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:02:21 +0700

    update: subcategory controller and service

--- src/core/sub-category/sub-category.controller.ts
+++ src/core/sub-category/sub-category.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { SubCategoryService } from './sub-category.service';
+import { CreateSubcategoryDto } from './subcategory.dto';
 
-@Controller('sub-category')
+@Controller('subcategory')
 export class SubCategoryController {
   constructor(private readonly subCategoryService: SubCategoryService) {}
+
+  @Get()
+  findAll() {
+    return this.subCategoryService.getAllSubcategories();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.subCategoryService.getSubcategoryById(id);
+  }
+
+  @Post()
+  create(@Body() data: CreateSubcategoryDto) {
+    return this.subCategoryService.createSubategory(data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.subCategoryService.deleteSubategory(id);
+  }
 }
--- src/core/sub-category/sub-category.service.ts
+++ src/core/sub-category/sub-category.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateSubcategoryDto } from './subcategory.dto';
 
 @Injectable()
-export class SubCategoryService {}
+export class SubCategoryService {
+  constructor(private readonly db: PrismaService) {}
+
+  async getAllSubcategories() {
+    return await this.db.subcategory.findMany({
+      include: { shoe: true, category: true },
+    });
+  }
+
+  async getSubcategoryById(id: number) {
+    return await this.db.subcategory.findUnique({ where: { id } });
+  }
+
+  async createSubategory(data: CreateSubcategoryDto) {
+    return await this.db.subcategory.create({ data });
+  }
+
+  async deleteSubategory(id: number) {
+    return await this.db.subcategory.delete({ where: { id } });
+  }
+}
