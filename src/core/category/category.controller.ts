d16b2c383a1d2eced9d7db13c3fa16d1c93af341
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:01:29 +0700

    feat: add category controller and service

--- src/core/category/category.controller.ts
+++ src/core/category/category.controller.ts
@@ -1,7 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { CreateCategoryDto } from './category.dto';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  findAll() {
+    return this.categoryService.getAllCategories();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.categoryService.getCategoryById(id);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/static/category',
+        filename(req, file, cb) {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  create(
+    @Body() data: CreateCategoryDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'image/*' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.categoryService.createCategory(data, file);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.categoryService.deleteCategory(id);
+  }
 }
--- src/core/category/category.service.ts
+++ src/core/category/category.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCategoryDto } from './category.dto';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(private readonly db: PrismaService) {}
+
+  async getAllCategories() {
+    return await this.db.category.findMany({
+      include: { shoe: true, subcategory: true },
+    });
+  }
+
+  async getCategoryById(id: number) {
+    return await this.db.category.findUnique({ where: { id } });
+  }
+
+  async createCategory(data: CreateCategoryDto, file: Express.Multer.File) {
+    return await this.db.category.create({
+      data: {
+        ...data,
+        imgUrl: file?.filename ? 'static/category/' + file?.filename : null,
+      },
+    });
+  }
+
+  async deleteCategory(id: number) {
+    return await this.db.category.delete({ where: { id } });
+  }
+}
