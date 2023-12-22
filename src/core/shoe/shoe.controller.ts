55e1ec88bd3b6467bd9eff70f90f8ee135a7e550
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:02:04 +0700

    update: shoe controller and service

--- src/core/shoe/shoe.controller.ts
+++ src/core/shoe/shoe.controller.ts
@@ -1,7 +1,57 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ShoeService } from './shoe.service';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { CreateShoeDto } from './shoe.dto';
 
 @Controller('shoe')
 export class ShoeController {
   constructor(private readonly shoeService: ShoeService) {}
+
+  @Get()
+  findAll() {
+    return this.shoeService.getAllProduct();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: number) {
+    return this.shoeService.getProductById(id);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FilesInterceptor('files', 3, {
+      storage: diskStorage({
+        destination: './uploads/static/shoe',
+        filename(req, file, cb) {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  create(
+    @Body() data: CreateShoeDto,
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 2 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'image/*' }),
+        ],
+      }),
+    )
+    files: Array<Express.Multer.File>,
+  ) {
+    return this.shoeService.createProduct(data, files);
+  }
 }
--- src/core/shoe/shoe.service.ts
+++ src/core/shoe/shoe.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { Shoe } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateShoeDto } from './shoe.dto';
 
 @Injectable()
-export class ShoeService {}
+export class ShoeService {
+  constructor(private readonly db: PrismaService) {}
+
+  async getAllProduct(): Promise<Shoe[]> {
+    return await this.db.shoe.findMany({
+      include: {
+        brand: true,
+        shoeImage: true,
+        subCategory: true,
+        stock: true,
+      },
+    });
+  }
+
+  async getProductById(id: number): Promise<Shoe> {
+    return await this.db.shoe.findUnique({ where: { id } });
+  }
+
+  async createProduct(
+    data: CreateShoeDto,
+    files: Express.Multer.File[],
+  ): Promise<Shoe> {
+    const shoe = await this.db.shoe.create({
+      data: {
+        name: data.name,
+        description: data.description,
+        price: Number(data.price),
+        weight: Number(data.weight),
+        status: data.status,
+        brandId: Number(data.brandId),
+        subcategoryId: Number(data.subcategoryId),
+        categoryId: Number(data.categoryId),
+      },
+    });
+
+    await this.db.shoeImage.createMany({
+      data: files.map((file) => {
+        return {
+          imgUrl: `static/shoe/${file.filename}`,
+          shoeId: Number(shoe.id),
+        };
+      }),
+    });
+
+    return shoe;
+  }
+}
