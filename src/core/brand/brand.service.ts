b19a7497c9b8b3e270899d113e5432fed914d07f
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:01:07 +0700

    feat: add brand service

--- src/core/brand/brand.service.ts
+++ src/core/brand/brand.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateBrandDto } from './brand.dto';
 
 @Injectable()
-export class BrandService {}
+export class BrandService {
+  constructor(private readonly db: PrismaService) {}
+
+  async getAllBrands() {
+    return await this.db.brand.findMany({ include: { shoe: true } });
+  }
+
+  async getBrandById(id: number) {
+    return await this.db.brand.findUnique({ where: { id } });
+  }
+
+  async createBrand(data: CreateBrandDto, file: Express.Multer.File) {
+    return await this.db.brand.create({
+      data: {
+        ...data,
+        imgUrl: file?.filename ? 'static/brand/' + file?.filename : null,
+      },
+    });
+  }
+
+  async deleteShoe(id: number) {
+    return await this.db.brand.delete({ where: { id } });
+  }
+}
