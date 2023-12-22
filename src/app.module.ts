96ad034b77ae6901ec8a9d035fbcefe706f03384
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 16:58:34 +0700

    update: add shoe, category, brand module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,20 @@ import { AuthModule } from './core/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ShoeModule } from './core/shoe/shoe.module';
+import { CategoryModule } from './core/category/category.module';
+import { BrandModule } from './core/brand/brand.module';
+import { SubCategoryModule } from './core/sub-category/sub-category.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
     PrismaModule,
+    ShoeModule,
+    CategoryModule,
+    BrandModule,
+    SubCategoryModule,
     ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
