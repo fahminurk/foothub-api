73c2cc87fcf905757541bb7df92b89e076f472a5
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:00:45 +0700

    feat: add user dto

---
+++ src/core/user/user.dto.ts
@@ -0,0 +1,56 @@
+import { Role } from '@prisma/client';
+import {
+  Contains,
+  IsString,
+  Length,
+  IsNumberString,
+  IsEnum,
+  IsNotEmpty,
+  IsEmail,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(8)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Contains('08')
+  @Length(11, 12)
+  @IsNumberString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role: Role;
+}
+
+export class UpdateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Contains('08')
+  @Length(11, 12)
+  @IsNumberString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role?: Role;
+}
