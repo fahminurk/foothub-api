d452a6bc8f886967dd743685ecc68fd3afaa23c3
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-22 17:00:26 +0700

    feat: add shoe dto

---
+++ src/core/shoe/shoe.dto.ts
@@ -0,0 +1,35 @@
+import { IsString, IsEnum, IsNotEmpty, IsNumberString } from 'class-validator';
+import { ShoeStatus } from '@prisma/client';
+
+export class CreateShoeDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  price: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  weight: number;
+
+  @IsEnum(ShoeStatus)
+  status: ShoeStatus;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  brandId: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  categoryId: number;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  subcategoryId: number;
+}
