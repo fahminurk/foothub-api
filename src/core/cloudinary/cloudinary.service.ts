aeac879e8b83c3bdb31a3fc2be33d1f137798061
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-23 21:53:44 +0700

    feat: add cloudinary for upload file

---
+++ src/core/cloudinary/cloudinary.service.ts
@@ -0,0 +1,27 @@
+// cloudinary.service.ts
+
+import { Injectable } from '@nestjs/common';
+import {
+  v2 as cloudinary,
+  UploadApiErrorResponse,
+  UploadApiResponse,
+} from 'cloudinary';
+import streamifier = require('streamifier');
+
+export type CloudinaryResponse = UploadApiResponse | UploadApiErrorResponse;
+
+@Injectable()
+export class CloudinaryService {
+  uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
+    return new Promise<CloudinaryResponse>((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        },
+      );
+
+      streamifier.createReadStream(file.buffer).pipe(uploadStream);
+    });
+  }
+}
