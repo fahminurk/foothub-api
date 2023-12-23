786e5e72cf057fd0f789318cda16357f73488dee
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-23 21:53:27 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './core/user/user.module';
 import { AuthModule } from './core/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { ShoeModule } from './core/shoe/shoe.module';
 import { CategoryModule } from './core/category/category.module';
 import { BrandModule } from './core/brand/brand.module';
 import { SubCategoryModule } from './core/sub-category/sub-category.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { CloudinaryModule } from './core/cloudinary/cloudinary.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { SubCategoryModule } from './core/sub-category/sub-category.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
     }),
+    CloudinaryModule,
   ],
   controllers: [],
   providers: [],
