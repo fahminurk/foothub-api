d170bed1c9ce293c2fd96c81e4cc3154d14c010e
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-23 21:54:27 +0700

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.use(helmet());
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({ origin: true, credentials: true });
 
   // app.setGlobalPrefix('api');
   app.useGlobalPipes(
