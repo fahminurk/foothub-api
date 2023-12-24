0afe6d89cc526dc033fd66f96f02aa554739d9f8
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-24 21:57:22 +0700

    update: add cloudinary module

--- src/core/brand/brand.module.ts
+++ src/core/brand/brand.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BrandService } from './brand.service';
 import { BrandController } from './brand.controller';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
   controllers: [BrandController],
   providers: [BrandService],
+  imports: [CloudinaryModule],
 })
 export class BrandModule {}
