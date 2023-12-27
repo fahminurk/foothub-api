9660ceb8eed61e85aea94af30f5cfaab9bc029bc
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-27 23:28:49 +0700

    update: add httpmodule

--- src/core/address/address.module.ts
+++ src/core/address/address.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
+import { CityProvinceModule } from '../city-province/city-province.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [AddressController],
   providers: [AddressService],
+  imports: [
+    CityProvinceModule,
+    HttpModule.register({
+      headers: { key: process.env.OPENCAGE_API_KEY },
+    }),
+  ],
 })
 export class AddressModule {}
