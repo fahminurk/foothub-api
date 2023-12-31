d7b3005f442f980d47336060b8df6f10869ed100
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-12-31 19:21:54 +0700

    update:  createstock

--- src/core/stock/stock.service.ts
+++ src/core/stock/stock.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateStockDto } from './stock.dto';
 
 @Injectable()
 export class StockService {
@@ -11,7 +12,16 @@ export class StockService {
     });
   }
 
-  async createStock(data: { shoeId: number; sizeId: number; stock: number }) {
+  async createStock(data: CreateStockDto) {
+    const existingStock = await this.db.stock.findFirst({
+      where: {
+        shoeId: Number(data.shoeId),
+        sizeId: Number(data.sizeId),
+      },
+    });
+
+    if (existingStock) throw new BadRequestException('Stock already exists');
+
     return await this.db.stock.create({
       data: {
         stock: Number(data.stock),
